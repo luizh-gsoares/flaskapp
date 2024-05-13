1e5cce9cc7f31bde7852a5828597b54061720b99
Luiz Henrique Soares <luizhenriquesoares18@hotmail.com>
2024-05-13 12:13:15 -0300

    Adicionando loin

--- flask_app.py
+++ flask_app.py
@@ -1,17 +1,27 @@
-import os
 from flask import Flask, render_template, session, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+import os
 
 app = Flask(__name__)
 
-app.secret_key = '123456'
-UPLOAD_FOLDER = '/home/luizhgsoares/mysite/upload'
+app.secret_key = 'PaoDoceFrango2024'
 
+UPLOAD_FOLDER = '/home/luizhgsoares/mysite/upload'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Configuração do banco de dados
+class Base(DeclarativeBase):
+  pass
+db = SQLAlchemy(model_class= Base)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///minhabase.sqlite3'
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
+app.app_context().push()
+
+# Definição da classe Usuario
 class Usuario(db.Model):
     __tablename__ = "usuarios"
     id = db.Column(db.Integer, primary_key=True)
@@ -22,25 +32,42 @@ class Usuario(db.Model):
         self.nome = nome
         self.senha = senha
 
+# Criação do banco de dados
+with app.app_context():
+    db.create_all()
+
+#
+# Rotas
+#
+
+# Rota para a página inicial
 @app.route('/')
 def index():
+    if 'username' in session:
+        return render_template('index.html', usuario_logado = session['username'], usuarios = Usuario.query.all())
+    
     return render_template('index.html')
 
-@app.route("/usuario", methods=['POST', 'GET'])
+# Rota para a página de registro
+@app.route("/register", methods=['GET'])
 def usuario():
-    return render_template('usuario.html')
+    return render_template('register.html')
 
-@app.route("/usuario/addUsuario", methods=['POST', 'GET'])
+# Rota para adicionar um usuário
+@app.route("/usuario/create", methods=['POST'])
 def addUsuario():
     if request.method == 'POST':
         nome = request.form['nome']
         senha = request.form['senha']
-        user = Usuario(nome, senha)
-        db.session.add(user)
-        db.session.commit()
-    users = Usuario.query.all()
-    return render_template('usuario.html')
+        if Usuario.query.filter_by(nome=nome).first():
+            return render_template('index.html', mensagem = 'Usuário já existe, tente novamente.')
+        else:
+            user = Usuario(nome, senha)
+            db.session.add(user)
+            db.session.commit()
+            return render_template('login.html', mensagem = 'Usuário criado com sucesso. Faça o login.')
 
+# Rota do login do usuário
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -49,15 +76,17 @@ def login():
         user = Usuario.query.filter_by(nome=nome).first()
         if user and user.senha == senha:
             session['username'] = nome
-            return 'Login feito com sucesso'
-        return 'Falha no login'
+            return render_template('index.html', usuario_logado = session['username'], usuarios = Usuario.query.all())
+        return render_template('login.html', mensagem = 'Usuário ou senha inválidos, tente novamente.')
     return render_template('login.html')
 
+# Rota para o logout
 @app.route('/logout')
 def logout():
     session.pop('username', None)
-    return redirect(url_for('index'))
+    return render_template('index.html')
 
+# Rota para a página de upload
 @app.route("/upload", methods=['POST', 'GET'])
 def upload():
     if 'username' in session:
@@ -65,10 +94,11 @@ def upload():
             file = request.files['arquivo']
             savePath = os.path.join(UPLOAD_FOLDER, file.filename)
             file.save(savePath)
-            return 'upload feito com sucesso'
+            return render_template('upload.html', mensagem = 'Arquivo enviado com sucesso.')
         return render_template('upload.html')
-    return 'Você não está logado'
+    return render_template('index.html', mensagem = 'Faça o login para acessar esta página.')
 
+# Função principal
 if __name__ == '__main__':
     db.create_all()
     app.run()
